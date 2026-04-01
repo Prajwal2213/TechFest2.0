1cebde21c088aa1cf1f93631e7fa116ad7392732
Pavankumargr2004 <parun4325@gmail.com>
2026-04-01 23:34:53 +0530

    updated countdowm

--- src/components/CountDown.jsx
+++ src/components/CountDown.jsx
@@ -25,7 +25,7 @@ const CountDown = () => {
   }, []);
 
 return (
-  <section className="w-full flex flex-col items-center justify-center py-16 ">
+  <section className="w-full flex flex-col items-center justify-center py-16 -mt-5">
     {/* Title */}
     <div className="bg-black/70 px-6 py-12 border-white">
 
