6f94ac011839435d7399a3b61c317e4b4e4de70c
Prajwal2213 <prajwalm2222@gmail.com>
2026-04-06 14:54:50 +0530

    updated images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
         bg-no-repeat
         bg-cover
         bg-center
-        md:bg-[url('https://res.cloudinary.com/dstbnmjwh/image/upload/v1771321472/image2_oc8zev.webp')]
+        md:bg-[url('https://res.cloudinary.com/duajsf7ft/image/upload/v1775467201/image2_oc8zev_jqzmhl.webp')]
       "
     >
       <div className="max-w-4xl space-y-6 mt-35 md:mt-20 lg:-mt-60 backdrop-blur-sm p-6 rounded-xl md:backdrop-blur-none relative z-10">
